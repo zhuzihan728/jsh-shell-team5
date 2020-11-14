beed95b00ce205e3989b0003085ca0ba8d9d0f51
zhuzihan728 <zcabhub@ucl.ac.uk>
2020-11-14 19:22:06 +0000

    Merge branch 'master' of https://github.com/comp0010/jsh-team5

--- src/main/java/uk/ac/ucl/jsh/applications/Cat.java
+++ src/main/java/uk/ac/ucl/jsh/applications/Cat.java
@@ -18,23 +18,33 @@ public class Cat implements Application{
 	@Override
     public void exec(ArrayList<String> appArgs, InputStream input, OutputStream output) throws IOException {
         OutputStreamWriter writer = new OutputStreamWriter(output);
+         
         if (appArgs.isEmpty()) {
-            throw new RuntimeException("cat: missing arguments");
+            try{
+                writeToShell(new Scanner(input), writer);
+            }
+            catch(IOException e){
+                throw new RuntimeException("cat: " + e.getMessage());
+            }
         } 
         else {
             for (String arg : appArgs) {
-                try (Scanner reader = InputReader.file_reader(arg)) {
-                    String line = null;
-                    while (reader.hasNextLine()) {
-                        line = reader.nextLine();
-                        writer.write(String.valueOf(line));
-                        writer.write(System.getProperty("line.separator"));
-                        writer.flush();
-                    }
+                try {
+                    writeToShell(InputReader.file_reader(arg), writer);
                 } 
                 catch (IOException e) {throw new RuntimeException("cat: cannot open " + arg);}
                 catch (RuntimeException e) {throw new RuntimeException("cat: file does not exist");}
             }
+        }
+    }
+
+    private void writeToShell( Scanner in, OutputStreamWriter writer) throws IOException {
+        String line;
+        while (in.hasNextLine()) {
+            line = in.nextLine();
+            writer.write(String.valueOf(line));
+            writer.write(System.getProperty("line.separator"));
+            writer.flush();
         }
     }
 }
