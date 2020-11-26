1ea1dcdd8b1ed225c791704b90264dbd9a2d3321
zixuandingkelly <zixuanding0515@outlook.com>
2020-11-26 23:09:27 +0800

    fixed problem for globbing

--- src/main/java/uk/ac/ucl/jsh/toolkit/Globbing.java
+++ src/main/java/uk/ac/ucl/jsh/toolkit/Globbing.java
@@ -1,6 +1,8 @@
 package uk.ac.ucl.jsh.toolkit;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,6 +10,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class Globbing {
+    private final String file_separator = System.getProperty("file.separator");
     private static final String currentDirectory = WorkingDr.getInstance().getWD();
     private ArrayList<String> globbingResult;
     private final String globbing_pattern;
@@ -20,15 +23,18 @@ public class Globbing {
 
     private void get_globbed(){
         globbingResult = new ArrayList<>();
-        Path dir = Paths.get(currentDirectory);
         DirectoryStream<Path> stream;
+        File file = new File(currentDirectory + file_separator + globbing_pattern);
+        Path dir = Paths.get(file.getParent());
         try {
-            stream = Files.newDirectoryStream(dir, globbing_pattern);
+            stream = Files.newDirectoryStream(dir, file.getName());
         }catch (IOException e){
             throw new RuntimeException(e.getMessage());
         }
+
         for (Path entry : stream) {
-            globbingResult.add(entry.getFileName().toString());
+            String relative_path = entry.toAbsolutePath().toString().replace(currentDirectory,"");
+            globbingResult.add(relative_path.substring(1));
         }
         if (globbingResult.isEmpty()) {
             globbingResult.add(globbing_pattern);
