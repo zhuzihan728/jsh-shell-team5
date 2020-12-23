f7397e88e4f7090cf6f7a782118d3b7a254fe44d
kellydingzx <55750344+kellydingzx@users.noreply.github.com>
2020-12-23 23:37:31 +0800

    add docs

--- src/main/java/uk/ac/ucl/jsh/toolkit/ParserErrorHandling.java
+++ src/main/java/uk/ac/ucl/jsh/toolkit/ParserErrorHandling.java
@@ -3,10 +3,23 @@ package uk.ac.ucl.jsh.toolkit;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.misc.ParseCancellationException;
 
-
+/**
+ * Class used to wrap the error from the Antlr plugin into a format that provides more information.
+ */
 public class ParserErrorHandling extends BaseErrorListener{
 
+    /**
+     * This public field provides an instance of this class which allows this class to be
+     * used without instantiate a class.
+     */
     public static final ParserErrorHandling INSTANCE = new ParserErrorHandling();
+
+    /**
+     * Method used to wrap up the default error message.
+     * 
+     * @param msg The raw command that cannot be parsed.
+     * @param charPositionInLine The position of the character where the error occurs.
+     */
     @Override
     public void syntaxError(Recognizer<?, ?> recognizer,
                             Object offendingSymbol,
