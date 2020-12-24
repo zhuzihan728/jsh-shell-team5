e3b5e516444e8fcfa780e6dd43cff3b203884bc9
Coco Liu <zcabiuf@ucl.ac.uk>
2020-12-24 15:28:59 +0800

    add doc for TestFile Handle

--- src/main/java/uk/ac/ucl/jsh/toolkit/TestFileHandle.java
+++ src/main/java/uk/ac/ucl/jsh/toolkit/TestFileHandle.java
@@ -5,8 +5,16 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-
+/**
+ * TestFileHandle class helps to create the file and delete file in the working directory of Jsh shell, it follows the singleton pattern
+ */
 public class TestFileHandle {
+    /**
+     * Utility function used to create a File Hierarchy that is used during the Test
+     * 
+     * @throws IOException Exception being thrown if the File hierearchy creation fail
+     */
+
     public void createTestFileHierarchy(String path, String fileName) throws IOException {
         String fileSeparator = System.getProperty("file.separator");
         // Check whether directory exists
@@ -16,6 +24,13 @@ public class TestFileHandle {
         } 
         Files.createFile(Paths.get(path + fileSeparator + fileName));
    }
+
+
+   /**
+    * Utility function used to delete a directory and all the files it contains
+    * 
+    * @param crtDirectory The directory that needs to be deleted
+    */
 
    public void deleteFileHierarchy(File crtDirectory) {
     File[] files = crtDirectory.listFiles();
